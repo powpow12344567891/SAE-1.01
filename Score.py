d2092b35a7b9de0fe16cd0b8489fe7c677e47160
powpow1234456789 <seydi.d@hotmail.fr>
2024-10-24 09:07:47 +0200

    noveau syteme de score

--- Score.py
+++ Score.py
@@ -1,5 +1,8 @@
-a = "lol"
- 
-fichier = open("score.txt")   
-fichier.write(a)        #Écrit la valeur de la variable a dans le fichier
-fichier.close()
+if __name__ == "__main__":
+    lecteur : str
+    scorej1 =str("1")
+    file = open("score.txt", "w", encoding="utf8")
+    file.write(scorej1)
+    lecteur = file.read()
+    file.close()
+    print(lecteur)
