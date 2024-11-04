ea6dee5217a563ae9fdcff5edcff18e201ee9e16
powpow12344567891 <seydi.d@hotmail.fr>
2024-11-04 18:29:34 +0100

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,1 +1,8 @@
-import pickle
+if __name__ == "__main__":
+    lecteur : str
+    scorej1 =str("1")
+    file = open("score.txt", "w", encoding="utf8")
+    file.write(scorej1)
+    lecteur = file.read()
+    file.close()
+    print(lecteur)
