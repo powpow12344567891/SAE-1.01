8fd3d426df170301fa5d84ce645b4de8bead6e70
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-06 15:55:03 +0100

    caa

--- Score.py
+++ Score.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if __name__ == "__ main__":
     lecteur1 : int = 110
     lecteur2 : int = 11
@@ -8,17 +7,5 @@ if __name__ == "__ main__":
     file.write(scorej1)
     file.write(scorej2)
     lecteur1 = file.read()
-    print(lecteur1)
-=======
-if __name__ == "__main__":
-    lecteur1 : str
-    lecteur2 : str
-    scorej1 =str("joueur1")
-    scorej2 = str("joueur2")
-    file = open("score.txt", "r+", encoding="utf8")
-    file.write(scorej1)
-    file.write("\n")
-    file.write(scorej2,)
-    lecteur1 = file.read()
->>>>>>> 0a011bdcf250456dfe74524ee4b552800f34f0f2
+    print(int(lecteur1))
     file.close()
