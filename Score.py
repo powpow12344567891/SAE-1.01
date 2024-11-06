a0d628c46900d604f7f887aa3bba90048e07d909
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-06 16:11:01 +0100

    powpow

--- Score.py
+++ Score.py
@@ -1,11 +1,16 @@
-if __name__ == "__ main__":
-    lecteur1 : int = 110
-    lecteur2 : int = 11
+if __name__ == "__main__":
+    lecteur1 : int 
+    lecteur2 : int 
+    scorej1 : str
+    scorej2 : str
+    lecteur1 = 100
+    lecteur2 = 200
     scorej1 =str(lecteur1)
+    
     scorej2 = str(lecteur2)
     file = open("score.txt", "r+", encoding="utf8")
     file.write(scorej1)
+    file.write("\n")
     file.write(scorej2)
     lecteur1 = file.read()
-    print(int(lecteur1))
     file.close()
