c9cdd57f4d51f820a78d565d592b64ff6cc33525
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-06 18:16:51 +0100

    ecriture + lecturce fonctionnel

--- Score.py
+++ Score.py
@@ -1,11 +1,18 @@
-if __name__ == "__main__":
+if __name__ =="__main__":
     scorej1:  int
     scorej2 : int
     lecteurscorej1 : str
     lecteurscorej2 : str
-    scorej1 = 100
-    scorej2 = 200
-    file = open("score.txt", "r+", encoding="utf8")
+    scorej1 = 800
+    scorej2 = 300
+    file = open("score.txt", "w", encoding="utf8")
     file.write(str(scorej1))
     file.write("\n")
     file.write(str(scorej2))
+    file.close()
+    file = open("score.txt", "r", encoding="utf8")  
+    lignes = int(file.readline())
+    print(lignes)
+    lignes2 = int(file.readline())
+    print(lignes2)
+    file.close()
