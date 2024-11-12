60ea3c03904d7dfdfee034cb0b52265d1b4cd329
powpow12344567891 <seydi.d@hotmail.fr>
2024-11-12 13:37:11 +0100

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,18 +1,23 @@
 if __name__ =="__main__":
     scorej1:  int
     scorej2 : int
+    ajoutscorej1 : int
+    ajoutscorej2 : int
     lecteurscorej1 : str
     lecteurscorej2 : str
-    scorej1 = 800
-    scorej2 = 300
-    file = open("score.txt", "w", encoding="utf8")
-    file.write(str(scorej1))
-    file.write("\n")
-    file.write(str(scorej2))
-    file.close()
-    file = open("score.txt", "r", encoding="utf8")  
-    lignes = int(file.readline())
-    print(lignes)
-    lignes2 = int(file.readline())
-    print(lignes2)
-    file.close()
+    
+    def recup_val_score(scorej1, scorej2):
+        file = open("score.txt", "r", encoding="utf8")  
+        scorej1 = int(file.readline())
+        scorej2 = int(file.readline())
+        print("le score du joueur 1 est :" , scorej1)
+        print("le score du joueur 2 est :" , scorej2)
+        file.close()
+        
+    def ajout_de_val_score(scorej1, scorej2, ajoutscorej1, ajoutscorej2):
+        file = open("score.txt", "w", encoding="utf8")
+        file.write(str(scorej1 + ajoutscorej1))
+        file.write("\n")
+        file.write(str(scorej2 + ajoutscorej2))
+        file.close()
+        
