e5a77f6cf52569c720b7ba6df615272e8ac369a6
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-18 16:11:58 +0100

    pow

--- morpion.py
+++ morpion.py
@@ -1,18 +1,26 @@
 if __name__ == "__main__":
     joueur1 : int
     joueur2 : int
+    choix : int
+    tour : int
     victoirej1 : bool
     victoirej2 : bool
     val : int
-    tableau1 :list[int]
+    tableau1 :list[str]
     victoirej1 = False
     victoirej2 = False
-    tableau1 =[0,0,0,0,0,0,0,0,0]
+    tour = 1
+    tableau1 =[" - "," - "," - " ," - "," - "," - " ," - "," - "," - "]
     val = 0
 def affichagemorpion():
     
     for i in range(0,3):
         print("\n")
+        
         for j in range(0,3):
-            print(tableau1[i], end='')    
             
+            print(tableau1[i], end='')    
+    
+affichagemorpion()
+choix = int(input)
+if()
