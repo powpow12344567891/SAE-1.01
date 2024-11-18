b78ac9b5ffc9d836f1b5ace12ce371277528ee54
powpow12344567891 <seydi.d@hotmail.fr>
2024-11-18 15:52:14 +0100

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,6 +1,18 @@
 if __name__ == "__main__":
+    joueur1 : int
+    joueur2 : int
+    victoirej1 : bool
+    victoirej2 : bool
+    val : int
     tableau1 :list[int]
-    tableau2 :list[list[int]]
-    tableau1 = [1,2,3]
-    tableau2 =[tableau1]
-    print(tableau2)
+    victoirej1 = False
+    victoirej2 = False
+    tableau1 =[0,0,0,0,0,0,0,0,0]
+    val = 0
+def affichagemorpion():
+    
+    for i in range(0,3):
+        print("\n")
+        for j in range(0,3):
+            print(tableau1[i], end='')    
+            
