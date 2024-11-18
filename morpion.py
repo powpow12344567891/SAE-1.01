5fea89fc22cfac01121fb2fa15e31ebac1c8c148
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-18 16:50:03 +0100

    caca

--- morpion.py
+++ morpion.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     tour : int
     victoirej1 : bool
     victoirej2 : bool
+    retourligne : int
     val : int
     tableau1 :list[str]
     victoirej1 = False
@@ -12,15 +13,37 @@ if __name__ == "__main__":
     tour = 1
     tableau1 =[" - "," - "," - " ," - "," - "," - " ," - "," - "," - "]
     val = 0
+    retourligne = 1
+9
 def affichagemorpion():
     
-    for i in range(0,3):
-        print("\n")
-        
-        for j in range(0,3):
-            
-            print(tableau1[i], end='')    
+    for i in range(0,9):
+        if ((i == 3) or (i == 6) or (i == 9)):
+            print("\n")
+        print(tableau1[i], end='')    
     
 affichagemorpion()
-choix = int(input)
-if()
+def choixdujoueur1():
+    print("\n")
+    choix = int(input("choix de la case joueur 1 "))
+    print("\n")
+    while (choix > 9) or ( tableau1[choix-1] != " - "):
+        print("\n", "choix non valide")
+        choix = int(input("choix de la case joueur 1 "))
+    print("\n")
+    tableau1[choix-1]  = str(" X ")
+    affichagemorpion()
+def choixdujoueur2():
+    print("\n")
+    choix = int(input("choix de la case joueur 1 "))
+    print("\n")
+    while (choix > 9) or ( tableau1[choix-1] != " - "):
+        print("\n", "choix non valide")
+        choix = int(input("choix de la case joueur 1 "))
+    print("\n")
+    tableau1[choix-1]  = str(" 0 ")
+    affichagemorpion()    
+
+while ((victoirej1==False) or (victoirej2==False)):
+    choixdujoueur1()
+    choixdujoueur2()
