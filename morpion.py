a3e3bf8739edd37ae5a7e091a3df5e2b10a98375
powpow1234456789 <seydi.d@hotmail.fr>
2024-11-19 18:37:07 +0100

    morpion fonctionnel uwu

--- morpion.py
+++ morpion.py
@@ -1,20 +1,30 @@
-if __name__ == "__main__":
-    joueur1 : int
-    joueur2 : int
-    choix : int
-    tour : int
-    victoirej1 : bool
-    victoirej2 : bool
-    retourligne : int
-    val : int
-    tableau1 :list[str]
-    victoirej1 = False
-    victoirej2 = False
-    tour = 1
-    tableau1 =[" - "," - "," - " ," - "," - "," - " ," - "," - "," - "]
-    val = 0
-    retourligne = 1
-9
+
+joueur1 : int
+joueur2 : int
+choix : int
+tour : int
+victoirej1 : bool
+victoirej2 : bool
+retourligne : int
+val : int
+tableau1 :list[str]
+victoirej1 = False
+victoirej2 = False
+tour = 1
+tableau1 =[" - "," - "," - " ," - "," - "," - " ," - "," - "," - "]
+lignes_gagnantes = [
+    [0, 1, 2],  # Ligne 1
+    [3, 4, 5],  # Ligne 2
+    [6, 7, 8],  # Ligne 3
+    [0, 3, 6],  # Colonne 1
+    [1, 4, 7],  # Colonne 2
+    [2, 5, 8],  # Colonne 3
+    [0, 4, 8],  # Diagonale principale
+    [2, 4, 6]   # Diagonale secondaire
+]
+val = 0
+retourligne = 1
+
 def affichagemorpion():
     
     for i in range(0,9):
@@ -22,8 +32,8 @@ def affichagemorpion():
             print("\n")
         print(tableau1[i], end='')    
     
-affichagemorpion()
 def choixdujoueur1():
+    affichagemorpion()
     print("\n")
     choix = int(input("choix de la case joueur 1 "))
     print("\n")
@@ -32,18 +42,49 @@ def choixdujoueur1():
         choix = int(input("choix de la case joueur 1 "))
     print("\n")
     tableau1[choix-1]  = str(" X ")
-    affichagemorpion()
 def choixdujoueur2():
+    affichagemorpion()
     print("\n")
-    choix = int(input("choix de la case joueur 1 "))
+    choix = int(input("choix de la case joueur 2 "))
     print("\n")
     while (choix > 9) or ( tableau1[choix-1] != " - "):
         print("\n", "choix non valide")
-        choix = int(input("choix de la case joueur 1 "))
+        choix = int(input("choix de la case joueur 2 "))
     print("\n")
     tableau1[choix-1]  = str(" 0 ")
-    affichagemorpion()    
+def victoire():
+        global victoirej1, victoirej2  # Utilisation des variables globales
 
-while ((victoirej1==False) or (victoirej2==False)):
-    choixdujoueur1()
-    choixdujoueur2()
+        # Vérification de toutes les combinaisons gagnantes
+        for ligne in lignes_gagnantes:
+            if tableau1[ligne[0]] == tableau1[ligne[1]] == tableau1[ligne[2]] and tableau1[ligne[0]] != " - ":
+                if tableau1[ligne[0]] == " X ":
+                    victoirej1 = True
+                elif tableau1[ligne[0]] == " 0 ":
+                    victoirej2 = True
+                    
+
+def morpion():
+    global tour
+    while ((victoirej1==False) and (victoirej2==False) and (tour <= 9)):
+        choixdujoueur1()
+        victoire()
+        tour = tour+1
+        if ((victoirej1==False) and (victoirej2==False) and (tour <= 9)):
+            choixdujoueur2()
+            victoire()
+            tour = tour+1
+
+    if(victoirej1==True):
+        print("Victoire du joueur 1")
+        affichagemorpion()
+
+    elif(victoirej2==True):
+            print("Victoire du joueur 2")
+            affichagemorpion()
+    else:
+        print("égalité")
+        affichagemorpion()
+    
+if __name__ == "__main__":
+    morpion()
