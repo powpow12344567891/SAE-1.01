0474ea682c8602a2a8e018623b57a78f65f1e7a3
Sovann <tourretsovann@gmail.com>
2024-12-12 18:09:17 +0100

    SI ALLUMETTE FINI ENCORE CASSÉ, JE ME COUPE LES VEINES.

--- allumette.py
+++ allumette.py
@@ -6,32 +6,33 @@ def allumette(joueur1: str, joueur2: str):
     perdant: int = 0  # Affiche le nombre initial d'allumettes
     print("Il reste", allumette, "allumettes.")
     print("|" * allumette)
-    while allumette > 0:  # Tour du joueur 1
+    while allumette > 0:  
+        # Tour du joueur 1
         print(f"{joueur1}, c'est votre tour.")
         choixj1 = demande_allumettes(f"{joueur1}, choisissez combien d'allumettes vous voulez prendre (entre 1 et 3) : ")
+        
         while choixj1 > allumette:
             print("Erreur, il n'y a pas la quantité d'allumette que vous soouhaité retirer.")
             choixj1 = demande_allumettes(f"{joueur1}, choisissez combien d'allumettes vous voulez prendre (entre 1 et 3) : ")
         allumette -= choixj1
+
         if allumette <= 0:
             print("Vous avez retiré la dernière allumette.")
             perdant = 1
             cond_vic(perdant, joueur1, joueur2)
-<<<<<<< HEAD
             break
 
-=======
->>>>>>> 2a72cd9e02e918cba849ed00546942c965c0fba1
         print(f"{joueur1} a pris {choixj1} allumettes.")
         print("Il reste", allumette, "allumettes.")
-        print("|" * allumette)         # Tour du joueur 2
+        print("|" * allumette)         
+        # Tour du joueur 2
         print(f"{joueur2}, c'est votre tour.")
         choixj2 = demande_allumettes(f"{joueur2}, choisissez combien d'allumettes vous voulez prendre (entre 1 et 3) : ")
+        
         while choixj2 > allumette:
             print("Erreur, il n'y a pas la quantité d'allumette que vous soouhaité retirer.")
             choixj2 = demande_allumettes(f"{joueur2}, choisissez combien d'allumettes vous voulez prendre (entre 1 et 3) : ")
         allumette -= choixj2
-<<<<<<< HEAD
 
         if allumette <= 0:
             print("Vous avez retiré la dernière allumette.")
@@ -45,14 +46,6 @@ def allumette(joueur1: str, joueur2: str):
 
 
 # Affichage des résultats
-=======
-        print(f"{joueur2} a pris {choixj2} allumettes.")
-        print("Il reste", allumette, "allumettes.")
-        print("|" * allumette)
-        if allumette <= 0:
-            perdant = 2
-            cond_vic(perdant, joueur1, joueur2)   # Affichage des résultats
->>>>>>> 2a72cd9e02e918cba849ed00546942c965c0fba1
 def cond_vic(perdant:int, joueur1: str, joueur2: str):
     if perdant == 1:
         ajoutscore[3] = 10
